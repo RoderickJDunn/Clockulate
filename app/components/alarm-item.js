df7276ad418aaf820bba5609b44bd1e8d66f22d4
RoderickJDunn <roderickjdunn@gmail.com>
2018-09-14 19:45:57 -0400

    Preliminary testing of alarm ringing animation

--- app/components/alarm-item.js
+++ app/components/alarm-item.js
@@ -25,6 +25,73 @@ import { TextStyle } from "../styles/text";
 import { ListStyle, AlarmListStyle } from "../styles/list";
 import { scaleByFactor } from "../util/font-scale";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+let shakeAnimMap = new Map([
+    [0, 0],
+    [0.1, -15],
+    [0.2, -26],
+    [0.3, -33],
+    [0.35, 33],
+    [0.4, -29],
+    [0.45, 25],
+    [0.5, -22],
+    [0.55, 19],
+    [0.6, -16],
+    [0.65, 13],
+    [0.7, 10],
+    [0.75, -8],
+    [0.8, 6],
+    [0.85, -4],
+    [0.9, 3],
+    [0.95, -2],
+    [1, 0]
+]);
+// let shakeAnimMap = new Map([
+//     [0, 0],
+//     [0.025, 10],
+//     [0.05, 20],
+//     [0.075, 30],
+//     [0.1, 40],
+//     [0.125, 50],
+//     [0.15, 60],
+//     [0.175, 70],
+//     [0.2, 80],
+//     [0.225, 90],
+//     [0.25, 100],
+//     [0.275, 110],
+//     [0.3, 120],
+//     [0.325, 130],
+//     [0.35, 140],
+//     [0.375, 150],
+//     [0.4, 160],
+//     [0.425, 170],
+//     [0.45, 180],
+//     [0.475, 190],
+//     [0.5, 200],
+//     [0.525, 210],
+//     [0.55, 220],
+//     [0.575, 230],
+//     [0.6, 240],
+//     [0.625, 250],
+//     [0.65, 260],
+//     [0.675, 270],
+//     [0.7, 280],
+//     [0.725, 290],
+//     [0.75, 300],
+//     [0.775, 310],
+//     [0.8, 320],
+//     [0.825, 330],
+//     [0.85, 340],
+//     [0.875, 350],
+//     [0.9, 360],
+//     [0.925, 360],
+//     [0.95, 360],
+//     [0.975, 360],
+//     [1, 360]
+// ]);
+
+let anchorPoint = [];
+function getTransformXForRotation(rotationMap) {}
 class AlarmItem extends React.PureComponent {
     /*
     Props: 
@@ -33,6 +100,11 @@ class AlarmItem extends React.PureComponent {
     width = Dimensions.get("window").width; //full width
     height = Dimensions.get("window").height; //full height
     _position = new Animated.Value(0);
+
+    _ringingAnimation = new Animated.Value(0);
+    _ringingScaleAnim = new Animated.Value(1);
+
+    _ringTransform = [];
     // _onPressItem = item => {
     //     // console.debug("_onPressItem called");
 
@@ -56,7 +128,8 @@ class AlarmItem extends React.PureComponent {
         this.state = {
             switchValue: props.alarm.enabled,
             animProgress: new Animated.Value(initAnimProgress),
-            isMoving: false
+            isMoving: false,
+            isRinging: false
         };
 
         this._appearAnim = new Animated.Value(0);
@@ -93,6 +166,12 @@ class AlarmItem extends React.PureComponent {
         } else {
             this._appearAnim.setValue(1);
         }
+
+        if (this.props.alarm.enabled) {
+            console.log("alarm is enabled");
+
+            // setTimeout(this._ringingAnimation.stopAnimation, 5000);
+        }
     }
 
     componentWillReceiveProps(props) {
@@ -101,6 +180,93 @@ class AlarmItem extends React.PureComponent {
             // console.log("Triggering animation");
             this._triggerAnimation(props.alarm.enabled, false);
         }
+
+        let shouldStopAnimation = false;
+        if (props.alarm.enabled == true) {
+            // check if we are 0-60 past the alarm wakeUpTime. If so, start 'ringing animation'.
+
+            let secSinceAlarm =
+                (moment() - moment(props.alarm.wakeUpTime)) / 1000;
+
+            if (secSinceAlarm > 0 && secSinceAlarm < 60) {
+                // only play "ringing" animation, if it has been <60 sec since alarm fired.
+                this.playRingingAnimation();
+                this.setState({ isRinging: true });
+            } else {
+                shouldStopAnimation = true;
+            }
+        } else {
+            shouldStopAnimation = true;
+        }
+
+        if (shouldStopAnimation) {
+            this._ringingAnimation.stopAnimation();
+            Animated.parallel([
+                Animated.timing(this._ringingAnimation, {
+                    toValue: 0,
+                    duration: 100,
+                    useNativeDriver: true
+                }),
+                Animated.timing(this._ringingScaleAnim, {
+                    toValue: 1,
+                    duration: 900,
+                    useNativeDriver: true
+                })
+            ]);
+        }
+    }
+
+    playRingingAnimation() {
+        Animated.loop(
+            Animated.parallel([
+                Animated.sequence([
+                    Animated.timing(this._ringingAnimation, {
+                        toValue: 1,
+                        duration: 1500,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(this._ringingAnimation, {
+                        toValue: 1,
+                        duration: 2000,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(this._ringingAnimation, {
+                        toValue: 0,
+                        duration: 0,
+                        useNativeDriver: true
+                    })
+                ]),
+                Animated.sequence([
+                    Animated.timing(this._ringingScaleAnim, {
+                        toValue: 0.9,
+                        duration: 450,
+                        useNativeDriver: true,
+                        easing: Easing.quad
+                    }),
+                    Animated.timing(this._ringingScaleAnim, {
+                        toValue: 1.2,
+                        duration: 300,
+                        useNativeDriver: true,
+                        easing: Easing.quad
+                    }),
+                    Animated.timing(this._ringingScaleAnim, {
+                        toValue: 1.2,
+                        duration: 500,
+                        useNativeDriver: true
+                    }),
+                    Animated.spring(this._ringingScaleAnim, {
+                        toValue: 1,
+                        bounciness: 20,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(this._ringingScaleAnim, {
+                        toValue: 1,
+                        duration: 900,
+                        useNativeDriver: true
+                    })
+                ])
+            ])
+        ).start();
     }
 
     _triggerAnimation(nextAlarmState, notifyParentOfToggle) {
@@ -138,6 +304,84 @@ class AlarmItem extends React.PureComponent {
             this.props.onToggle(this.props.alarm);
         }
         this.setState({ switchValue: nextAlarmState });
+    }
+
+    _calcShakeAnim() {
+        // rad = angle * Math.PI / 180
+        // transformX(Math.cos(angle) * dx - Math.sin(angle) * dy)
+        // transformY(Math.sin(angle) * dx + Math.cos(angle) * dy)
+        // rotate(angle+"deg")
+        this._ringTransform = [];
+
+        this._ringTransform.push({
+            scale: this._ringingScaleAnim
+        });
+
+        this._rotate = {
+            rotate: this._ringingAnimation.interpolate({
+                inputRange: Array.from(shakeAnimMap.keys()),
+                outputRange: Array.from(shakeAnimMap.values()).map(angle => {
+                    return angle + "deg";
+                })
+            })
+        };
+
+        let CENTER = { x: 34, y: 42 };
+        let NEW_CENTER = { x: 37.16, y: 44.66 };
+
+        this._translateX = {
+            translateX: this._ringingAnimation.interpolate({
+                inputRange: Array.from(shakeAnimMap.keys()),
+                outputRange: Array.from(shakeAnimMap.values()).map(angle => {
+                    let rad = (angle * Math.PI) / 180;
+
+                    let rPoint = { x: 0, y: 0 };
+                    rPoint.x =
+                        CENTER.x +
+                        (NEW_CENTER.x - CENTER.x) * Math.cos(rad) -
+                        (NEW_CENTER.y - CENTER.y) * Math.sin(rad);
+                    // rPoint.x =
+                    //     32.83 +
+                    //     (36.83 - 32.83) * Math.cos(rad) -
+                    //     (44.33 - 41.5) * Math.sin(rad);
+                    // let quadrantFactor = 1;
+                    // return Math.cos(rad) * 5.5 - Math.sin(rad) * 1;
+                    // return 4.4 - 2.69 * Math.cos(rad);
+
+                    // return rPoint.x;
+                    return -1 * (rPoint.x - NEW_CENTER.x);
+                })
+            })
+        };
+
+        this._translateY = {
+            translateY: this._ringingAnimation.interpolate({
+                inputRange: Array.from(shakeAnimMap.keys()),
+                outputRange: Array.from(shakeAnimMap.values()).map(angle => {
+                    let rad = (angle * Math.PI) / 180;
+                    // let quadrantFactor = 1;
+
+                    // return Math.sin(rad) * 5.5 - Math.cos(rad) * 1;
+                    // return 1 + 2.69 * Math.sin(rad);
+
+                    let rPoint = { x: 0, y: 0 };
+                    rPoint.y =
+                        CENTER.y +
+                        (NEW_CENTER.x - CENTER.x) * Math.sin(rad) +
+                        (NEW_CENTER.y - CENTER.y) * Math.cos(rad);
+
+                    return -1 * (rPoint.y - NEW_CENTER.y);
+                    // return rPoint.y;
+                })
+            })
+        };
+        this._ringTransform.push(this._translateY);
+        this._ringTransform.push(this._translateX);
+        this._ringTransform.push(this._rotate);
+
+        console.log(this._rotate.rotate._config.outputRange);
+        console.log(this._translateX.translateX._config.outputRange);
+        console.log(this._translateY.translateY._config.outputRange);
     }
 
     render() {
@@ -432,13 +676,36 @@ class AlarmItem extends React.PureComponent {
                                 }
                             ]}
                             onStartShouldSetResponder={evt => true}
+                            // onPress={e => {
+                            //     console.log(e.nativeEvent);
+                            // }}
                             onPress={this._triggerAnimation.bind(
                                 this,
                                 !this.state.switchValue,
                                 true
                             )}
                         >
-                            <View
+                            <Animated.View
+                                onLayout={({ nativeEvent }) => {
+                                    let {
+                                        x,
+                                        y,
+                                        width,
+                                        height
+                                    } = nativeEvent.layout;
+                                    console.log("Lottie Wrapper layout:");
+                                    console.log("width", width);
+                                    console.log("height", height);
+
+                                    this._lottieCenterX = width / 2;
+                                    this._lottieCenterY = height / 2;
+
+                                    this._lottiePivotX =
+                                        this._lottieCenterX + 20;
+                                    this._lottiePivotY = this._lottieCenterY;
+                                    this._calcShakeAnim();
+                                    // this.setState(this.state);
+                                }}
                                 style={[
                                     {
                                         flex:
@@ -450,17 +717,125 @@ class AlarmItem extends React.PureComponent {
                                         // backgroundColor: "green",
                                         alignSelf: "stretch",
                                         alignContent: "center",
-                                        justifyContent: "center"
+                                        justifyContent: "center",
+                                        transform: this._ringTransform
+                                        // transform: [
+                                        //     { translateX: 2.5 },
+                                        //     { translateY: -1.8 },
+                                        //     { rotate: "45deg" }
+                                        // ]
+                                        // transform: [
+                                        //     { translateX: 5.5 },
+                                        //     { translateY: -1.1 },
+                                        //     { rotate: "90deg" }
+                                        // ]
+                                        // transform: [
+                                        //     { translateX: 7 },
+                                        //     { translateY: 4 },
+                                        //     { rotate: "180deg" }
+                                        // ]
+                                        // transform: [
+                                        //     { translateX: 1.1 },
+                                        //     { translateY: 5.5 },
+                                        //     { rotate: "270deg" }
+                                        // ]
+
+                                        // [
+                                        //     this._rotate
+                                        // {
+                                        //     translateY: this._ringingAnimation.interpolate(
+                                        //         {
+                                        //             inputRange: Array.from(
+                                        //                 shakeAnimMap.keys()
+                                        //             ),
+                                        //             outputRange: [
+                                        //                 0,
+                                        //                 -1,
+                                        //                 1,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0
+                                        //             ]
+                                        //         }
+                                        //     )
+                                        // },
+                                        // {
+                                        //     translateX: this._ringingAnimation.interpolate(
+                                        //         {
+                                        //             inputRange: Array.from(
+                                        //                 shakeAnimMap.keys()
+                                        //             ),
+                                        //             outputRange: [
+                                        //                 0,
+                                        //                 2,
+                                        //                 -2,
+                                        //                 1,
+                                        //                 -1,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0,
+                                        //                 0
+                                        //             ]
+                                        //         }
+                                        //     )
+                                        // }
+                                        //     {
+                                        //         scale: this._ringingScaleAnim
+                                        //     }
+                                        // ]
                                     }
                                 ]}
                             >
+                                {/* <View
+                                    style={{
+                                        position: "absolute",
+                                        left: "50%", // 32.83
+                                        width: 1,
+                                        height: 80,
+                                        backgroundColor: "red"
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        position: "absolute",
+                                        left: 35.5,
+                                        width: 1,
+                                        height: 80,
+                                        backgroundColor: "red"
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        position: "absolute",
+                                        top: "50%", // 41.5
+                                        width: 60,
+                                        height: 1,
+                                        backgroundColor: "red"
+                                    }}
+                                />
+                                <View
+                                    style={{
+                                        position: "absolute",
+                                        top: 43,
+                                        width: 60,
+                                        height: 1,
+                                        backgroundColor: "red"
+                                    }}
+                                /> */}
                                 <LottieView
                                     source={require("../img/off-to-clock-lottie.json")}
                                     progress={this.state.animProgress}
                                     resizeMode={"contain"}
                                     style={[StyleSheet.absoluteFill]}
                                 />
-                            </View>
+                            </Animated.View>
                         </TouchableOpacity>
                         <View style={AlarmListStyle.infoContainer}>
                             <Text
