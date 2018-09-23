d622187dd608ece9e8b5ea5677a645b5e7e4b802
RoderickJDunn <roderickjdunn@gmail.com>
2018-09-23 00:16:43 -0400

    Added animation to underscore line of mode 'styled-radio' in menu

--- app/components/styled-radio.js
+++ app/components/styled-radio.js
@@ -4,31 +4,54 @@ import {
     TouchableWithoutFeedback,
     View,
     TouchableOpacity,
-    Text
+    Text,
+    Animated
 } from "react-native";
 import Colors from "../styles/colors";
 
 class StyledRadio extends React.Component {
     constructor(props) {
         super(props);
-        console.log("StyledRadio constructor ");
-        console.log("props", props);
+        // console.log("StyledRadio constructor ");
+        // console.log("props", props);
         this.state = {
-            selectedIdx: props.initialIdx
+            selectedIdx: props.initialIdx,
+            width: null
         };
+
+        this._underscorAnim = new Animated.Value(props.initialIdx);
     }
 
-    // componentWillReceiveProps(props) {
-    //     console.log("componentWillReceiveProps");
-    //     this.setState({ selectedIdx: props.selectedIdx });
-    // }
+    componentWillReceiveProps(props) {
+        console.log("componentWillReceiveProps");
+        if (props.selectedIdx != this.props.selectedIdx)
+            this.setState({ selectedIdx: props.selectedIdx });
+    }
+
+    onLayout = event => {
+        const { width } = event.nativeEvent.layout;
+        this.setState({ width: width });
+    };
+
+    _onSelectItem(idx) {
+        this.setState({ selectedIdx: idx });
+        Animated.spring(this._underscorAnim, {
+            toValue: idx,
+            tension: 300,
+            friction: 11,
+            useNativeDriver: true
+        }).start();
+        this.props.onSelect(idx);
+    }
 
     render() {
-        console.log("Styled Radio");
-        console.log("this.props", this.props);
-        console.log("this.state", this.state);
+        let { width } = this.state;
+        // console.log("Styled Radio");
+        // console.log("this.props", this.props);
+        // console.log("this.state", this.state);
         return (
             <View
+                onLayout={this.onLayout}
                 style={{
                     flexDirection: "row",
                     justifyContent: "space-between",
@@ -44,16 +67,15 @@ class StyledRadio extends React.Component {
                         paddingBottom: 10
                     }}
                     onPress={() => {
-                        this.setState({ selectedIdx: 0 });
-                        this.props.onSelect(0);
+                        this._onSelectItem(0);
                     }}
                 >
                     <Text style={{ alignSelf: "center" }}>
                         {this.props.options[0].name}
                     </Text>
-                    {this.state.selectedIdx == 0 && (
+                    {/* {this.state.selectedIdx == 0 && (
                         <View style={[styles.underscore]} />
-                    )}
+                    )} */}
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={{
@@ -63,16 +85,15 @@ class StyledRadio extends React.Component {
                         paddingBottom: 10
                     }}
                     onPress={() => {
-                        this.setState({ selectedIdx: 1 });
-                        this.props.onSelect(1);
+                        this._onSelectItem(1);
                     }}
                 >
                     <Text style={{ alignSelf: "center" }}>
                         {this.props.options[1].name}
                     </Text>
-                    {this.state.selectedIdx == 1 && (
+                    {/* {this.state.selectedIdx == 1 && (
                         <View style={[styles.underscore]} />
-                    )}
+                    )} */}
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={{
@@ -82,17 +103,37 @@ class StyledRadio extends React.Component {
                         paddingBottom: 10
                     }}
                     onPress={() => {
-                        this.setState({ selectedIdx: 2 });
-                        this.props.onSelect(2);
+                        this._onSelectItem(2);
                     }}
                 >
                     <Text style={{ alignSelf: "center" }}>
                         {this.props.options[2].name}
                     </Text>
-                    {this.state.selectedIdx == 2 && (
+                    {/* {this.state.selectedIdx == 2 && (
                         <View style={[styles.underscore]} />
-                    )}
+                    )} */}
                 </TouchableOpacity>
+                <Animated.View
+                    style={[
+                        styles.underscore,
+                        {
+                            transform: [
+                                {
+                                    translateX: this._underscorAnim.interpolate(
+                                        {
+                                            inputRange: [0, 1, 2],
+                                            outputRange: [
+                                                width * 0.01,
+                                                width * 0.335,
+                                                width * 0.67
+                                            ]
+                                        }
+                                    )
+                                }
+                            ]
+                        }
+                    ]}
+                />
             </View>
         );
     }
@@ -107,8 +148,12 @@ const styles = StyleSheet.create({
         position: "absolute",
         height: 2,
         left: 0,
-        right: 0,
-        bottom: 10
+        // right: 0,
+        width: "32%",
+        bottom: 10,
+        shadowColor: "#9c75f0",
+        shadowRadius: 5,
+        shadowOpacity: 0.5
     }
 });
 
