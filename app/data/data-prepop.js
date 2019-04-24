3fc6c6bf740a921557a032dafa8323286a0f2f6e
RoderickJDunn <roderickjdunn@gmail.com>
2019-04-24 14:59:20 -0400

    - Added better prepop Tasks, and Alarm. - Also fixed prepop Alarm to have the correct wakeUpTime

--- app/data/data-prepop.js
+++ app/data/data-prepop.js
@@ -17,16 +17,16 @@ console.log("dummy data file");
 
 let prePopTasks = [
     {
-        name: "Take out trash and recycling (blue bin)",
+        name: "Take out trash",
         defaultDuration: 600
     },
     {
-        name: "Take out recyling (black bin)",
+        name: "Take out recyling",
         defaultDuration: 300
     },
     {
-        name: "Make egg sandwhich",
-        defaultDuration: 180
+        name: "Eat breakfast",
+        defaultDuration: 900
     },
     {
         name: "Eat cereal",
@@ -34,8 +34,80 @@ let prePopTasks = [
     },
     {
         name: "Snooze Alarm",
+        defaultDuration: 600
+    },
+    {
+        name: "Walk the dog",
+        defaultDuration: 900
+    },
+    {
+        name: "Go for a run",
+        defaultDuration: 1800
+    },
+    {
+        name: "Work out",
+        defaultDuration: 3600
+    },
+    {
+        name: "Take a shower",
+        defaultDuration: 600
+    },
+    {
+        name: "Drive to Work",
+        defaultDuration: 1800
+    },
+    {
+        name: "Take the Bus to work",
+        defaultDuration: 2700
+    },
+    {
+        name: "Take the Subway to work",
+        defaultDuration: 2700
+    },
+    {
+        name: "Bike to work",
+        defaultDuration: 1200
+    },
+    {
+        name: "Walk to work",
+        defaultDuration: 1200
+    },
+    {
+        name: "Drive to appointment",
+        defaultDuration: 1800
+    },
+    {
+        name: "Feed the pets",
+        defaultDuration: 300
+    },
+    {
+        name: "Get ready for work",
+        defaultDuration: 1800
+    },
+    {
+        name: "Pack a lunch",
+        defaultDuration: 300
+    },
+    {
+        name: "Make coffee",
+        defaultDuration: 300
+    },
+    {
+        name: "Brush my teeth",
+        defaultDuration: 180
+    },
+    {
+        name: "Prepare for meeting",
+        defaultDuration: 1800
+    },
+    {
+        name: "Drop kids off at school",
         defaultDuration: 1200
     }
+    // {
+    //     name: "",
+    //     defaultDuration:
+    // },
 ];
 
 let prePopSettings = [
@@ -54,54 +126,6 @@ let prePopSettings = [
     {
         name: "defaultShowHrsOfSleep",
         enabled: true
-    }
-];
-
-let prePopAdvStats = [
-    {
-        name: "dateAppLastOpened"
-    },
-    {
-        name: "appOpenedCountTotal"
-    },
-    {
-        name: "appOpenedCountSinceNUP", // NUP == Non-Use-Period
-        statType: ADV_STAT_TYPES.NUP_COUNT
-    },
-    {
-        name: "AlarmsListVCTotal" // VC == View Count
-    },
-    {
-        name: "AlarmsListVCSinceNUP",
-        statType: ADV_STAT_TYPES.NUP_COUNT
-    },
-    {
-        name: "MainMenuVCTotal" // VC == View Count
-    },
-    {
-        name: "MainMenuVCSinceNUP",
-        statType: ADV_STAT_TYPES.NUP_COUNT
-    },
-    {
-        name: "AlarmDetailVCTotal" // VC == View Count
-    },
-    {
-        name: "AlarmDetailVCSinceNUP",
-        statType: ADV_STAT_TYPES.NUP_COUNT
-    },
-    {
-        name: "TaskDetailVCTotal" // VC == View Count
-    },
-    {
-        name: "TaskDetailVCSinceNUP",
-        statType: ADV_STAT_TYPES.NUP_COUNT
-    },
-    {
-        name: "SoundsVCTotal" // VC == View Count
-    },
-    {
-        name: "SoundsVCSinceNUP",
-        statType: ADV_STAT_TYPES.NUP_COUNT
     }
 ];
 let now = moment();
@@ -132,61 +156,15 @@ function insertPrepopData() {
         }
 
         /**** Create Tasks *****/
-        console.log("Create Tasks");
-
-        const task1 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Walk the dogs",
-            defaultDuration: 600
-        });
-        const task2 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Clean kitchen",
-            defaultDuration: 1200
-        });
-        const task3 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Take a shower (wash hair)",
-            defaultDuration: 1200
-        });
-        const task4 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Take a shower",
-            defaultDuration: 900
-        });
-        const task5 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Biking (exercise)",
-            defaultDuration: 3600
-        });
-        const task6 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Strength training (exercise)",
-            defaultDuration: 1800
-        });
-        const task7 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Feed the pets",
-            defaultDuration: 180
-        });
-        const task8 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Travel to work",
-            defaultDuration: 1800
-        });
-        const task9 = realm.create("Task", {
-            id: uuid.v1(),
-            name: "Travel to appointment",
-            defaultDuration: 1200
-        });
-
-        /* Create some more tasks */
+        let tasks = [];
         for (let index = 0; index < prePopTasks.length; index++) {
-            realm.create("Task", {
-                id: uuid.v1(),
-                name: prePopTasks[index].name,
-                defaultDuration: prePopTasks[index].defaultDuration
-            });
+            tasks.push(
+                realm.create("Task", {
+                    id: uuid.v1(),
+                    name: prePopTasks[index].name,
+                    defaultDuration: prePopTasks[index].defaultDuration
+                })
+            );
         }
 
         /* Create Settings */
@@ -198,7 +176,6 @@ function insertPrepopData() {
                 value: prePopSettings[index].value
             });
         }
-
 
         /* Create Fake Sleep Disturbances, and LogDates to group them by day */
         // console.log("dummy disturbances");
@@ -287,86 +264,82 @@ function insertPrepopData() {
         // AlarmTasks for alarm1
         const almTask1 = realm.create("AlarmTask", {
             id: uuid.v1(),
-            task: task1,
+            task: tasks[2],
             order: 0,
-            duration: 600
+            duration: 900
             // Since I'm not specifying 'duration' or 'enabled' values, the defaults will be used (see DataSchemas.js
             // to check the default values).
         });
-        const almTask7 = realm.create("AlarmTask", {
+        const almTask2 = realm.create("AlarmTask", {
             id: uuid.v1(),
-            task: task7,
+            task: tasks[6],
             order: 1,
-            duration: 180
+            duration: 1800
         });
         const almTask3 = realm.create("AlarmTask", {
             id: uuid.v1(),
-            task: task3,
+            task: tasks[8],
             order: 2,
-            duration: 1200
+            duration: 600
         });
-        const almTask8 = realm.create("AlarmTask", {
+        const almTask4 = realm.create("AlarmTask", {
             id: uuid.v1(),
-            task: task8,
+            task: tasks[9],
             order: 3,
             duration: 1800
         });
 
         // AlarmTasks for alarm2
-        const almTask9 = realm.create("AlarmTask", {
-            id: uuid.v1(),
-            task: task9,
-            order: 3,
-            duration: 1200
-        });
-        const almTask6 = realm.create("AlarmTask", {
-            id: uuid.v1(),
-            task: task6,
-            order: 1,
-            duration: 1200
-        });
-        const almTask3b = realm.create("AlarmTask", {
-            id: uuid.v1(),
-            task: task3,
-            order: 2,
-            duration: 1200
-        });
-        const almTask1b = realm.create("AlarmTask", {
-            id: uuid.v1(),
-            task: task1,
-            order: 0,
-            duration: 600
-        });
+        // const almTask9 = realm.create("AlarmTask", {
+        //     id: uuid.v1(),
+        //     task: task9,
+        //     order: 3,
+        //     duration: 1200
+        // });
+        // const almTask6 = realm.create("AlarmTask", {
+        //     id: uuid.v1(),
+        //     task: task6,
+        //     order: 1,
+        //     duration: 1200
+        // });
+        // const almTask3b = realm.create("AlarmTask", {
+        //     id: uuid.v1(),
+        //     task: task3,
+        //     order: 2,
+        //     duration: 1200
+        // });
+        // const almTask1b = realm.create("AlarmTask", {
+        //     id: uuid.v1(),
+        //     task: task1,
+        //     order: 0,
+        //     duration: 600
+        // });
 
         let aSound1 = new AlarmSound();
-        let aSound2 = new AlarmSound();
+        // let aSound2 = new AlarmSound();
         const alarmSound1 = realm.create("AlarmSound", {
             id: aSound1.id,
             sound: aSound1.sound,
             type: aSound1.type
         });
-        const alarmSound2 = realm.create("AlarmSound", {
-            id: aSound2.id,
-            sound: aSound2.sound,
-            type: aSound2.type
-        });
 
         /**** Create Alarms *****/
         console.log("Adding dummy alarms");
 
-        let wake1 = moment("8:35", "HH:mm").toDate();
+        let wake1 = moment("7:50", "HH:mm").toDate();
         wake1 = DateUtils.date_to_nextTimeInstance(wake1);
 
-        let arrive1 = moment("9:35", "HH:mm").toDate();
+        let arrive1 = moment("9:15", "HH:mm").toDate();
         arrive1 = DateUtils.date_to_nextTimeInstance(arrive1);
 
         const alarm1 = realm.create("Alarm", {
             id: uuid.v1(),
-            wakeUpTime: wake1,
+            wakeUpTime: wake1, // NOTE: (TODO:) This time MUST match that calculated based on the alarmTasks and arrival time.
+            //              Otherwise, it will be incorrect, until the first re-render.
             arrivalTime: arrive1,
             mode: "autocalc", // TODO: Change back to basic for testing
-            tasks: [almTask1, almTask7, almTask3, almTask8],
-            label: "Wake up on work-day",
+            tasks: [almTask1, almTask2, almTask3, almTask4],
+            label: "Normal week day",
             status: ALARM_STATES.OFF, // if true, it is active, and will Ring at wakeUpTime.
             visible: true, // if true, this Alarm will appear in 'Alarms list' page. If false it won't appear, and if preset also false, will be entirely deleted.
             preset: false, // if tr
@@ -375,26 +348,26 @@ function insertPrepopData() {
             snoozeTime: DefaultAlarm().snoozeTime
         });
 
-        let wake2 = moment("10:00", "HH:mm").toDate();
-        wake2 = DateUtils.date_to_nextTimeInstance(wake2);
+        // let wake2 = moment("10:00", "HH:mm").toDate();
+        // wake2 = DateUtils.date_to_nextTimeInstance(wake2);
 
-        let arrive2 = moment("11:30", "HH:mm").toDate();
-        arrive2 = DateUtils.date_to_nextTimeInstance(arrive2);
+        // let arrive2 = moment("11:30", "HH:mm").toDate();
+        // arrive2 = DateUtils.date_to_nextTimeInstance(arrive2);
 
-        const alarm2 = realm.create("Alarm", {
-            id: uuid.v1(),
-            wakeUpTime: wake2,
-            arrivalTime: arrive2,
-            mode: "autocalc",
-            tasks: [almTask6, almTask9, almTask1b, almTask3b],
-            label: "Wake up for appointment",
-            status: ALARM_STATES.OFF, // if true, it is active, and will Ring at wakeUpTime.
-            visible: true, // if true, this Alarm will appear in 'Alarms list' page. If false it won't appear, and if preset also false, will be entirely deleted.
-            preset: false, // if tr
-            order: 1,
-            alarmSound: alarmSound2,
-            snoozeTime: DefaultAlarm().snoozeTime
-        });
+        // const alarm2 = realm.create("Alarm", {
+        //     id: uuid.v1(),
+        //     wakeUpTime: wake2,
+        //     arrivalTime: arrive2,
+        //     mode: "autocalc",
+        //     tasks: [almTask6, almTask9, almTask1b, almTask3b],
+        //     label: "Wake up for appointment",
+        //     status: ALARM_STATES.OFF, // if true, it is active, and will Ring at wakeUpTime.
+        //     visible: true, // if true, this Alarm will appear in 'Alarms list' page. If false it won't appear, and if preset also false, will be entirely deleted.
+        //     preset: false, // if tr
+        //     order: 1,
+        //     alarmSound: alarmSound2,
+        //     snoozeTime: DefaultAlarm().snoozeTime
+        // });
         // console.log(alarm2, alarm1, task2, task4, task5);
         console.log("Inserted pre-pop data");
     });
